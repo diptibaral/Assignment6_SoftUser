5fbcc6ec61cecd72c3716b1b4272abf364c65ef3
[Dipti Baral] <[premaswoorupa@gmail.com]>
2021-01-17 12:52:23 +0545

    Initial Commit

---
+++ app/src/main/java/com/dipti/week6assignment1softuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dipti.week6assignment1softuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
